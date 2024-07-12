c8e45c1ddcff10d5ff067823fca9929809b622b1
FT1006 <frankthp1234@gmail.com>
2024-07-12 18:05:02 +0800

    C: add char counter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text =  get_book_text(path) 
     num_word = get_words_num(text)
     print(f"{num_word} words in this book")
+    print(get_char_count(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +13,15 @@ def get_book_text(path):
 def get_words_num(text):
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    lowered_string = text.lower()
+    count_char = {}
+    for char in lowered_string: 
+        if char in count_char:
+            count_char[char] += 1
+        else:
+            count_char[char] = 1 
+    return count_char
 
 main()
